db080ae3e5c46eae3417c1b15a34c48ddb5cfd86
Carolina Freitas <99994934+CarolinaSFreitas@users.noreply.github.com>
2023-10-06 11:28:51 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 import express from 'express'
-import routes from '/routes.js'
 import { sequelize } from './database/conecta.js'
 import { Cliente } from './models/Cliente.js'
 import { Produto } from './models/Produto.js'
 import { Compra } from './models/Compra.js'
 import { ProdutosCompra } from './models/ProdutosCompra.js'
 import cors from 'cors'
+import router from './routes.js'
 
 const app = express()
 const port = 3000
 
 app.use(express.json())
 app.use(cors())
-app.use(routes)
+app.use(router)
 // app.use(express.static('images'));
 
 
@@ -24,7 +24,7 @@ async function conecta_db() {
     await Cliente.sync({ alter: true})          //A MAE PRECISA CRIAR PRIMEIRO PQ ELA É A DONA DA FOREIGN KEY - vai ciar a tabela no banco(se nao existir já) e permitir alterações de campos 
     console.log("Tabela de Clientes: Ok!")
 
-    await Produto.sync()         //vai ciar a tabela no banco(se nao existir já) e permitir alterações de campos 
+    await Produto.sync({alter: true})         //vai ciar a tabela no banco(se nao existir já) e permitir alterações de campos 
     console.log("Tabela de Produtos: Ok!")
 
     await Compra.sync({alter: true})         //vai ciar a tabela no banco(se nao existir já) e permitir alterações de campos 
